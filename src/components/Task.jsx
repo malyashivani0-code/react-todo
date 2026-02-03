15bc22a93b666084aeab4a8624f24bedf3a68f78
malyashivani0-code <malyashivani0@gmail.com>
2026-02-03 09:34:23 +0530

    Refactor Task component for better UI and logic
    Updated the Task component to improve layout and functionality.

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useEffect} from 'react'
+// import {Add} from './Add.jsx'
 
 function Task() {
   const [todos, setTodos] = useState([]);
@@ -19,8 +20,8 @@ function Task() {
   };
 
   const handelDelete = (index) => {
-    const isConfirm = confirm("Are you sure you want to delete this todo?");
-    if (!isConfirm) return;
+    const Confirm = confirm("Are you sure you want to delete this todo?");
+    if (!Confirm) return;
 
     let newTodos = [...todos];
     newTodos.splice(index, 1);
@@ -30,20 +31,20 @@ function Task() {
   };
 
   return (
-    <div className='flex flex-col justify-center max-w-xl mx-auto my-5 bg-violet-100 rounded-lg p-5'>
+    <div className='flex flex-col justify-center max-w-xl shadow-xl/20 mx-auto mt-20 my-5 bg-violet-100 rounded-lg p-5'>
       <h1 className='text-xl font-bold mb-4'>Your Todos</h1>
 
-      {todos.length === 0 && (
+     {todos.length === 0 && (
         <p className='text-gray-500'>No Todos Available.</p>
       )}
 
       {todos.map((todo, index) => (
-        <div key={index} className='flex gap-2 items-start mb-3'>
+        <div key={index} className='flex items-start mb-3 gap-3'>
           <ul
             onClick={() => toggleTodo(index)}
-            className='flex gap-2 w-full cursor-pointer'
+            className='flex gap-2 flex-1 min-w-0 cursor-pointer'
           >
-            <span className='flex justify-center items-center'>
+            <span className='flex justify-center items-center shrink-0'>
               {todo.completed ? (
                 <img  src="/check.png" />
               ) : (
@@ -51,23 +52,22 @@ function Task() {
               )}
             </span>
 
-            <li className='border-b border-gray-300 py-2 w-full'>
+            <li className='border-b border-gray-300 py-2 w-full min-w-0'>
               <p
-                className={`text-lg font-medium ${
-                  todo.completed ? "line-through text-gray-500" : ""
-                }`}
+                className={`text-lg font-medium truncate ${todo.completed ? "line-through text-gray-500" : ""}`}
               >
                 {todo.title}
               </p>
-              <p className={`text-sm text-gray-600 ${todo.completed ? "line-through" : ""}`}>
+              <p 
+              className={`text-sm text-gray-600 wrap-break-word ${todo.completed ? "line-through" : ""}`}>
                 {todo.description}
               </p>
             </li>
           </ul>
-
+        
           <button
             onClick={() => handelDelete(index)}
-            className='bg-violet-800 hover:bg-violet-900 px-2 py-2 font-bold text-white rounded-lg'
+            className='bg-violet-800 shrink-0 hover:bg-violet-950 mt-2 px-2 py-2 font-bold text-white cursor-pointer rounded-lg'
           >
             Delete
           </button>
